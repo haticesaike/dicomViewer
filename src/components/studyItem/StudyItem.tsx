17aa8b98a3c6ccf7cbaa21128bc547b11a3c9fa3
Hatice Saike <haticesaike374@gmail.com>
2024-08-23 04:20:31 +0300

    mockData added & studyList style edited

--- src/components/studyItem/StudyItem.tsx
+++ src/components/studyItem/StudyItem.tsx
@@ -1,7 +1,30 @@
-function StudyItem() {
-    return (
-        <div>StudyItem</div>
-    );
+import React from 'react';
+
+interface PatientItemProps {
+    patient: {
+        patientName: string;
+        mrn: string;
+        startDate: string;
+        endDate: string;
+        description: string;
+        modality: string;
+        accession: string;
+        instances: number;
+    };
 }
+
+const StudyItem: React.FC<PatientItemProps> = ({patient}) => {
+    return (
+        <div style={{border: '1px solid #ccc', padding: '10px', margin: '10px 0'}}>
+            <h3>{patient.patientName}</h3>
+            <p><strong>MRN:</strong> {patient.mrn}</p>
+            <p><strong>Study Period:</strong> {patient.startDate} - {patient.endDate}</p>
+            <p><strong>Description:</strong> {patient.description}</p>
+            <p><strong>Modality:</strong> {patient.modality}</p>
+            <p><strong>Accession:</strong> {patient.accession}</p>
+            <p><strong>Instances:</strong> {patient.instances}</p>
+        </div>
+    );
+};
 
 export default StudyItem;
--- src/components/studyList/StudyList.tsx
+++ src/components/studyList/StudyList.tsx
@@ -1,7 +1,61 @@
-function StudyList() {
-    return (
-        <div>StudyList</div>
-    );
-}
+import React from 'react';
+import {Accordion} from '@mantine/core';
+import {mockPatientData} from '../../data/MockData.tsx';
+import StudyItem from "../studyItem/StudyItem.tsx";
+import styles from './StudyList.module.css';
 
-export default StudyList;
+const PatientList: React.FC = () => {
+    return (
+        <Accordion
+            transitionDuration={300}
+            chevronPosition="left"
+            radius="md"
+            styles={() => ({
+                item: {
+                    border: `2px solid var(--indigo-purple)`,
+                    borderBottom: 'none',
+                    '&[data-active]': {
+                        borderColor: 'var(--black)',
+                    },
+                },
+                control: {
+                    color: 'var(--white)',
+                    padding: '5px',
+                    backgroundColor: 'var(--primary)',
+                    borderBottom: 'none',
+                    '&:hover': {
+                        backgroundColor: 'var(--primary)',
+                    },
+
+                },
+                panel: {
+                    backgroundColor: 'var(--blue-dark)',
+                    color: 'var(--white)',
+                    borderTop: 'none',
+                },
+            })}
+        >
+            {mockPatientData.map((patient) => (
+                <Accordion.Item value={patient.id.toString()} key={patient.id}>
+                    <Accordion.Control>
+                        <div className={styles.accordionControl}>
+                            <span>{patient.patientName}</span>
+                            <span>{patient.mrn}</span>
+                            <span>{patient.startDate}</span>
+                            <span>{patient.description}</span>
+                            <span>{patient.modality}</span>
+                            <span>{patient.accession}</span>
+                            <span>{patient.instances}</span>
+                        </div>
+                    </Accordion.Control>
+                    <Accordion.Panel>
+                        <StudyItem patient={patient}/>
+                    </Accordion.Panel>
+                </Accordion.Item>
+            ))}
+        </Accordion>
+
+    );
+};
+
+export default PatientList;
